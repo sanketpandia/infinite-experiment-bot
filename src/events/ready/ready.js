9f1fc9f4f031d8f0259b20b6172dd62e4669a736
Sanket Pandia <sanketpandia@gmail.com>
2024-02-21 02:47:06 +0530

    Rich presence

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -1,3 +1,8 @@
 module.exports = (client) => {
-    console.log(`${client.user.username} is online!`)
-}
+  console.log(`${client.user.username} is online!`);
+
+  client.user.setPresence({
+    activities: [{ name: "We're back! /live_new is up!" }],
+    status: "online",
+  });
+};
